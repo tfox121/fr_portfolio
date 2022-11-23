bf3544abc8bf7655c803eb03d96a9879f3a82db3
tfox121 <thomas.fox@compassdigital.io>
2022-11-23 11:05:25 -0500

    feat: add admin bar

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import React, { useContext, useState } from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
@@ -8,23 +9,36 @@ import yaml from 'js-yaml';
 import matter from 'gray-matter';
 import { serialize } from 'next-mdx-remote/serialize';
 import { MDXRemote } from 'next-mdx-remote';
+import AppBar from '@mui/material/AppBar';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
+import Toolbar from '@mui/material/Toolbar';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 import config from '../config.json';
 import { listTags } from '../src/lib/tags';
 import { fetchPostContent, slugToPostContent } from '../src/lib/posts';
 import { ContactForm } from '../src/components';
+import { UserContext } from '../src/hooks';
 
 export default function Home({ tags, work }) {
+  const router = useRouter();
+  const [anchorEl, setAnchorEl] = useState(null);
   const [selectedTags, setselectedTags] = useState(tags.map((tag) => tag.slug));
+  const { isAuthenticated, logout } = useContext(UserContext);
 
-  // useEffectOnceWhen(() => {
-  //   setselectedTags(tags.map((tag) => tag.slug));
-  // }, !!tags.length);
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleClick = (clickedTag) => {
     const selected = selectedTags.includes(clickedTag);
@@ -36,102 +50,143 @@ export default function Home({ tags, work }) {
   };
 
   return (
-    <Container maxWidth="md">
+    <Box>
       <Head>
         <title>Frances Richens - Portfolio</title>
       </Head>
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1">
-          {config.site_title}
-        </Typography>
-        <Box my={3}>
-          <Typography variant="p">{config.site_introduction}</Typography>
+      {isAuthenticated && (
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar variant="dense">
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Admin
+              </Typography>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={() => router.push('/admin')}>Admin</MenuItem>
+                <MenuItem onClick={logout}>Logout</MenuItem>
+              </Menu>
+            </Toolbar>
+          </AppBar>
         </Box>
-        <Box>
-          <Stack direction="row" spacing={1}>
-            {tags.map((tag) => {
-              const selected = selectedTags.includes(tag.slug);
+      )}
+      <Container maxWidth="md">
+        <Box
+          sx={{
+            my: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant="h4" component="h1">
+            {config.site_title}
+          </Typography>
+          <Box my={3}>
+            <Typography variant="p">{config.site_introduction}</Typography>
+          </Box>
+          <Box>
+            <Stack direction="row" spacing={1}>
+              {tags.map((tag) => {
+                const selected = selectedTags.includes(tag.slug);
+                return (
+                  <Chip
+                    key={tag.slug}
+                    label={tag.name}
+                    onClick={() => handleClick(tag.slug)}
+                    color={selected ? 'primary' : 'secondary'}
+                    variant={selected ? 'filled' : 'outlined'}
+                    sx={{
+                      padding: 0.5,
+                    }}
+                  />
+                );
+              })}
+            </Stack>
+          </Box>
+          <Box>
+            {work.map((item) => {
+              let tagEnabled = false;
+              item.scope.tags.forEach((tag) => {
+                if (selectedTags.includes(tag)) {
+                  tagEnabled = true;
+                }
+              });
+
+              if (!tagEnabled) return null;
+
               return (
-                <Chip
-                  key={tag.slug}
-                  label={tag.name}
-                  onClick={() => handleClick(tag.slug)}
-                  color={selected ? 'primary' : 'secondary'}
-                  variant={selected ? 'filled' : 'outlined'}
-                  sx={{
-                    padding: 0.5,
-                  }}
-                />
+                <React.Fragment key={item.scope.slug}>
+                  <Box my={2}>
+                    <Typography variant="h5" fontWeight={800} component="h2">
+                      {item.scope.title}
+                    </Typography>
+                    <Typography variant="subtitle2">
+                      {item.scope.description}
+                    </Typography>
+                    <MDXRemote {...item} scope={item.scope} />
+                    <Stack direction="row" spacing={1}>
+                      {item.scope.tags.map((tagSlug) => {
+                        const selected = selectedTags.includes(tagSlug);
+                        const tagName = tags.filter(
+                          (tag) => tag.slug === tagSlug,
+                        )[0].name;
+                        return (
+                          <Chip
+                            key={tagSlug}
+                            label={tagName}
+                            color={selected ? 'primary' : 'secondary'}
+                            variant={selected ? 'filled' : 'outlined'}
+                            size="small"
+                            sx={{
+                              fontSize: 12,
+                              height: 26,
+                              padding: 0.5,
+                            }}
+                          />
+                        );
+                      })}
+                    </Stack>
+                  </Box>
+                  <Divider variant="middle" />
+                </React.Fragment>
               );
             })}
-          </Stack>
+          </Box>
+          <ContactForm />
+          <Box mt={4}>
+            {/* TODO: make button link to config.linkedin_account_url */}
+            <IconButton>
+              <LinkedInIcon fontSize="large" color="primary" />
+            </IconButton>
+          </Box>
         </Box>
-        <Box>
-          {work.map((item) => {
-            let tagEnabled = false;
-            item.scope.tags.forEach((tag) => {
-              if (selectedTags.includes(tag)) {
-                tagEnabled = true;
-              }
-            });
-
-            if (!tagEnabled) return null;
-
-            return (
-              <React.Fragment key={item.scope.slug}>
-                <Box my={2}>
-                  <Typography variant="h5" fontWeight={800} component="h2">
-                    {item.scope.title}
-                  </Typography>
-                  <Typography variant="subtitle2">
-                    {item.scope.description}
-                  </Typography>
-                  <MDXRemote {...item} scope={item.scope} />
-                  <Stack direction="row" spacing={1}>
-                    {item.scope.tags.map((tagSlug) => {
-                      const selected = selectedTags.includes(tagSlug);
-                      const tagName = tags.filter(
-                        (tag) => tag.slug === tagSlug,
-                      )[0].name;
-                      return (
-                        <Chip
-                          key={tagSlug}
-                          label={tagName}
-                          color={selected ? 'primary' : 'secondary'}
-                          variant={selected ? 'filled' : 'outlined'}
-                          size="small"
-                          sx={{
-                            fontSize: 12,
-                            height: 26,
-                            padding: 0.5,
-                          }}
-                        />
-                      );
-                    })}
-                  </Stack>
-                </Box>
-                <Divider variant="middle" />
-              </React.Fragment>
-            );
-          })}
-        </Box>
-        <ContactForm />
-        <Box mt={4}>
-          {/* TODO: make button link to config.linkedin_account_url */}
-          <IconButton>
-            <LinkedInIcon fontSize="large" color="primary" />
-          </IconButton>
-        </Box>
-      </Box>
-    </Container>
+      </Container>
+    </Box>
   );
 }
 
