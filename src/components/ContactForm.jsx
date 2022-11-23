5719391d283a8cd8ee78cde37e4b8aa02a943c12
tfox121 <thomas.fox@compassdigital.io>
2022-11-22 21:43:51 -0500

    chore: adjust contact form styling

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import Alert from '@mui/material/Alert';
 
 const encode = (data) =>
@@ -66,72 +67,69 @@ function ContactForm() {
       onSubmit={handleSubmit}
       noValidate
       width="100%"
+      maxWidth="400px"
       my={3}
       name="contact"
       netlify="true"
     >
-      <Grid container spacing={2}>
-        <Grid item xs={0} md={3} />
-        <Grid item xs={12} md={3}>
-          <TextField
-            label="Your Name"
-            type="text"
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            error={errors.includes('name')}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} md={3}>
-          <TextField
-            label="Your Email"
-            type="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            error={errors.includes('email')}
-            fullWidth
-            required
-          />
-        </Grid>
-        <Grid item xs={0} md={3} />
-        <Grid item xs={0} md={3} />
-        <Grid item xs={12} md={6}>
-          <TextField
-            label="Message"
-            name="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            error={errors.includes('message')}
-            fullWidth
-            multiline
-            rows={3}
-            required
-          />
-        </Grid>
-        <Grid item xs={0} md={3} />
-
-        {formState !== 'pending' && (
-          <>
-            <Grid item xs={0} md={3} />
-            <Grid item xs={12} md={6}>
-              <Alert severity={formState}>
-                {errors.includes('failed')
-                  ? 'Message failed to send, please try again.'
-                  : formMessages[formState]}
-              </Alert>
+      <Typography variant="h5" component="h2" gutterBottom>
+        Contact me
+      </Typography>
+      <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing={2}>
+            <Grid item width="100%">
+              <TextField
+                label="Your Name"
+                type="text"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                error={errors.includes('name')}
+                fullWidth
+              />
             </Grid>
-            <Grid item xs={0} md={3} />
-          </>
-        )}
-        <Grid item xs={0} md={3} />
-        <Grid item xs={12} md={6}>
-          <Button type="submit" size="large" variant="contained" fullWidth>
-            Send
-          </Button>
+            <Grid item width="100%">
+              <TextField
+                label="Your Email"
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                error={errors.includes('email')}
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid item width="100%">
+              <TextField
+                label="Message"
+                name="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                error={errors.includes('message')}
+                fullWidth
+                multiline
+                rows={3}
+                required
+              />
+            </Grid>
+            {formState !== 'pending' && (
+              <Grid item width="100%">
+                <Alert severity={formState}>
+                  {errors.includes('failed')
+                    ? 'Message failed to send, please try again.'
+                    : formMessages[formState]}
+                </Alert>
+              </Grid>
+            )}
+            <Grid item width="80%">
+              <Button type="submit" size="large" variant="contained" fullWidth>
+                Send
+              </Button>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs={0} md={3} />
       </Grid>
     </Box>
   );
