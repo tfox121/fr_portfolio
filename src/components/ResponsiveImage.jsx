0602c33a6e9bc8be8c31253b47082c3f2501f406
tfox121 <thomas.fox@compassdigital.io>
2022-11-25 18:32:27 -0500

    refactor: update image sizing and add a border radius

--- src/components/ResponsiveImage.jsx
+++ src/components/ResponsiveImage.jsx
@@ -7,21 +7,25 @@ export default function ResponsiveImage({ src }) {
   const styles = {
     [theme.breakpoints.down('sm')]: {
       height: '200px',
+      width: '300px',
     },
     [theme.breakpoints.between('sm', 'lg')]: {
       height: '250px',
+      width: '375px',
     },
     '@media (min-width: 1200px)': {
       height: '300px',
+      width: '450px',
     },
   };
   return (
-    <Box width="100%" position="relative" mt={2} sx={styles}>
+    <Box position="relative" mt={2} sx={styles}>
       <Image
         src={src}
         fill
         style={{
           objectFit: 'contain',
+          borderRadius: '1em',
         }}
       />
     </Box>
