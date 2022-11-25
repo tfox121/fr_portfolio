fb8b056a7bf1f25d78f54b191fec3d38e14620e4
Tom Fox <51032384+tfox121@users.noreply.github.com>
2022-11-24 23:15:03 -0500

    refactor: adjust global styles and move components to wrap

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,12 @@
 import { useEffectOnceWhen } from 'rooks';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import Container from '@mui/material/Container';
+
 import { UserContext, useNetlifyAuth } from '../src/hooks';
+import { AdminToolbar } from '../src/components';
+
 import theme from '../src/theme';
 
 function MyApp({ Component, pageProps }) {
@@ -15,10 +20,16 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles
+        styles={{ body: { height: '100vh' }, '#__next': { height: '100%' } }}
+      />
       <UserContext.Provider value={netlifyAuth}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+        <AdminToolbar />
+        <Container maxWidth="md" sx={{ height: '100%' }}>
+          <Component {...pageProps} />
+        </Container>
       </UserContext.Provider>
     </ThemeProvider>
   );
