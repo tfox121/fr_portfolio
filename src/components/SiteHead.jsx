483db25d1fac93eca3bd16566d1e86bc983c54c3
Tom Fox <51032384+tfox121@users.noreply.github.com>
2022-11-24 23:15:48 -0500

    chore: adjust title composition

--- src/components/SiteHead.jsx
+++ src/components/SiteHead.jsx
@@ -1,8 +1,10 @@
 import Head from 'next/head';
 import React from 'react';
 
-export default function SiteHead({ siteMetadata }) {
-  const siteKeywords = siteMetadata.site_keywords.reduce(
+import config from '../../config.json';
+
+export default function SiteHead({ pageTitle }) {
+  const siteKeywords = config.site_metadata.site_keywords.reduce(
     (acc, currVal, index) => {
       if (index === 0) return currVal.keyword;
       return `${acc}, ${currVal.keyword}`;
@@ -12,9 +14,12 @@ export default function SiteHead({ siteMetadata }) {
 
   return (
     <Head>
-      <title>{siteMetadata.site_title}</title>
-      <meta name="description" content={siteMetadata.site_description} />
-      <meta name="author" content={siteMetadata.author} />
+      <title>{`${config.site_metadata.site_title} - ${pageTitle}`}</title>
+      <meta
+        name="description"
+        content={config.site_metadata.site_description}
+      />
+      <meta name="author" content={config.site_metadata.author} />
       <meta name="keywords" content={siteKeywords} />
     </Head>
   );
