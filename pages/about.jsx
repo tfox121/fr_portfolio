b6940f3d2c2cceb885d8a278cf2fc9a96acb0b1c
tfox121 <thomas.fox@compassdigital.io>
2022-12-30 18:16:15 -0500

    feat: parse markdown correctly in about text

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import { MDXRemote } from 'next-mdx-remote';
+import { serialize } from 'next-mdx-remote/serialize';
 
 import { PageHeading, ResponsiveImage, SiteHead } from '../src/components';
 
 import config from '../config.json';
 
-export default function About() {
+export default function About({ aboutMdx }) {
   return (
     <Container maxWidth="md" sx={{ height: '100%' }}>
       <Box my={4}>
@@ -15,7 +17,7 @@ export default function About() {
         <PageHeading />
         <Box display="flex" alignItems="center" flexDirection="column">
           <Typography variant="p" align="center">
-            {config.about}
+            <MDXRemote {...aboutMdx} />
           </Typography>
           <ResponsiveImage
             src={config.profile}
@@ -26,4 +28,9 @@ export default function About() {
       </Box>
     </Container>
   );
+}
+
+export async function getStaticProps() {
+  const aboutMdx = await serialize(config.about);
+  return { props: { aboutMdx } };
 }
