9baeb234b1fab87ee7de7cff1e7f53732d8f8f93
tfox121 <thomas.fox@compassdigital.io>
2022-12-30 19:11:52 -0500

    feat: use MUI Typography component as default for MDX

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,22 @@
 import { useEffectOnceWhen } from 'rooks';
+import Typography from '@mui/material/Typography';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import GlobalStyles from '@mui/material/GlobalStyles';
+import { MDXProvider } from '@mdx-js/react';
 
 import { UserContext, useNetlifyAuth } from '../src/hooks';
 import { AdminToolbar } from '../src/components';
 
 import theme from '../src/theme';
+
+const components = {
+  p: (props) => (
+    <Typography gutterBottom variant="p" component="p">
+      {props.children}
+    </Typography>
+  ),
+};
 
 function MyApp({ Component, pageProps }) {
   const netlifyAuth = useNetlifyAuth();
@@ -22,12 +32,14 @@ function MyApp({ Component, pageProps }) {
       <GlobalStyles
         styles={{ body: { height: '100vh' }, '#__next': { height: '100%' } }}
       />
-      <UserContext.Provider value={netlifyAuth}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <AdminToolbar />
-        <Component {...pageProps} />
-      </UserContext.Provider>
+      <MDXProvider components={components}>
+        <UserContext.Provider value={netlifyAuth}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <AdminToolbar />
+          <Component {...pageProps} />
+        </UserContext.Provider>
+      </MDXProvider>
     </ThemeProvider>
   );
 }
