bbf66feef13790f34a149395e7623ff2afc6f964
tfox121 <thomas.fox@compassdigital.io>
2022-12-30 19:13:06 -0500

    chore: reverse image and text positions

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
@@ -17,20 +16,16 @@ export default function About({ aboutMdx }) {
         <SiteHead pageTitle="About" />
         <PageHeading />
         <Grid container columns={11}>
-          <Grid item xs="auto">
-            <Box height="100%" display="flex" alignItems="center">
-              <ResponsiveImage
-                src={config.profile}
-                alt="Frances Richens"
-                priority
-              />
-            </Box>
-          </Grid>
-          <Grid xs={1} />
           <Grid item xs={5}>
-            <Typography variant="p">
-              <MDXRemote {...aboutMdx} />
-            </Typography>
+            <MDXRemote {...aboutMdx} />
+          </Grid>
+          <Grid item xs={1} />
+          <Grid item xs="auto">
+            <ResponsiveImage
+              src={config.profile}
+              alt="Frances Richens"
+              priority
+            />
           </Grid>
         </Grid>
       </Box>
