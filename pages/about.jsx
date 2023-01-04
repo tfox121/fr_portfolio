853e433be26ed00f5f4571c82be4e3686b872856
tfox121 <thomas.fox@compassdigital.io>
2023-01-04 15:25:09 -0500

    chore: adjust about section layout on smaller screens

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { MDXRemote } from 'next-mdx-remote';
 import { serialize } from 'next-mdx-remote/serialize';
 
@@ -10,22 +11,35 @@ import { PageHeading, ResponsiveImage, SiteHead } from '../src/components';
 import config from '../config.json';
 
 export default function About({ aboutMdx }) {
+  const smallDisplay = useMediaQuery((theme) => theme.breakpoints.down('md'));
+
   return (
     <Container maxWidth="md" sx={{ height: '100%' }}>
       <Box py={5}>
         <SiteHead pageTitle="About" />
         <PageHeading />
-        <Grid container columns={11}>
-          <Grid item xs={5}>
+        <Grid
+          container
+          columns={11}
+          direction={smallDisplay ? 'column-reverse' : 'row'}
+        >
+          <Grid
+            item
+            md={5}
+            xs={11}
+            textAlign={smallDisplay ? 'center' : 'left'}
+          >
             <MDXRemote {...aboutMdx} />
           </Grid>
-          <Grid item xs={1} />
+          <Grid item md={1} xs={0} />
           <Grid item xs="auto">
-            <ResponsiveImage
-              src={config.profile}
-              alt="Frances Richens"
-              priority
-            />
+            <Box display="flex" justifyContent="center" mb={2}>
+              <ResponsiveImage
+                src={config.profile}
+                alt="Frances Richens"
+                priority
+              />
+            </Box>
           </Grid>
         </Grid>
       </Box>
