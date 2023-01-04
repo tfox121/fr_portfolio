1853b89c47718f840aeaa1d4450ee25d5994f032
tfox121 <thomas.fox@compassdigital.io>
2023-01-03 22:32:59 -0500

    feat: add useWindowDimensions hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useNetlifyAuth } from './useNetlifyAuth';
 export { default as UserContext } from './UserContext';
+export { default as useWindowDimensions } from './useWindowDimensions';
---
+++ src/hooks/useWindowDimensions.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+function getWindowDimensions() {
+  if (typeof window === 'undefined') {
+    return {
+      width: 0,
+      height: 0,
+    };
+  }
+  return {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions(),
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
